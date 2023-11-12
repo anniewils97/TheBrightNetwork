d1d84be43be0a904184098f41c00cf2066bc4977
Annie Wilson <annemarie.wilson@yahoo.com>
2023-11-12 12:00:45 +0000

    created Spring boot project

---
+++ the_bright_network/src/main/java/com/bnta/the_bright_network/TheBrightNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.bnta.the_bright_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBrightNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBrightNetworkApplication.class, args);
+	}
+
+}
---
+++ the_bright_network/src/test/java/com/bnta/the_bright_network/TheBrightNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnta.the_bright_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBrightNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
