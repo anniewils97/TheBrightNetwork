cc0bcb3c16d4b4c1f1aba267194c8586aa7fd680
Faiz Fazaluddin <faiz.mf07@gmail.com>
2023-11-13 18:03:17 +0000

    edited DataLoader to make the data more consistent.

--- the_bright_network/src/main/java/com/bnta/the_bright_network/components/DataLoader.java
+++ the_bright_network/src/main/java/com/bnta/the_bright_network/components/DataLoader.java
@@ -47,10 +47,10 @@ public class DataLoader implements ApplicationRunner {
         userRepository.save(user4);
         userRepository.save(user5);
 
-        ChatRoom chatroom1 = new ChatRoom (" cohort 12 chatroom");
+        ChatRoom chatroom1 = new ChatRoom (" cohort 12 study group");
         ChatRoom chatroom2 = new ChatRoom (" cohort 11 chatroom");
         ChatRoom chatroom3 = new ChatRoom (" cohort 7 chatroom");
-        ChatRoom chatroom4 = new ChatRoom (" cohort 3 chatroom");
+        ChatRoom chatroom4 = new ChatRoom (" Trainers with c12");
 
         chatRoomRepository.save(chatroom1);
         chatRoomRepository.save(chatroom2);
@@ -58,24 +58,36 @@ public class DataLoader implements ApplicationRunner {
         chatRoomRepository.save(chatroom4);
 
         Subscription subscription1 = new Subscription(user1,chatroom1);
-        Subscription subscription2 = new Subscription(user2,chatroom2);
-        Subscription subscription3 = new Subscription(user3,chatroom4);
-        Subscription subscription4 = new Subscription(user2,chatroom1);
+        Subscription subscription2 = new Subscription(user2,chatroom1);
+        Subscription subscription3 = new Subscription(user3,chatroom1);
+        Subscription subscription4 = new Subscription(user4,chatroom1);
 
         subscriptionRepository.save(subscription1);
         subscriptionRepository.save(subscription2);
         subscriptionRepository.save(subscription3);
         subscriptionRepository.save(subscription4);
 
-        Message message1 = new Message("I'm Sarah !", subscription1,LocalDateTime.now());
+        Subscription subscription5 = new Subscription(user5,chatroom4);
+        Subscription subscription6 = new Subscription(user2,chatroom4);
+        Subscription subscription7 = new Subscription(user4,chatroom4);
+        Subscription subscription8 = new Subscription(user1,chatroom4);
+
+        subscriptionRepository.save(subscription5);
+        subscriptionRepository.save(subscription6);
+        subscriptionRepository.save(subscription7);
+        subscriptionRepository.save(subscription8);
+
+        Message message1 = new Message("I'm Sarah !", subscription2,LocalDateTime.now());
         Message message2 = new Message("Hi, everyone", subscription1,LocalDateTime.now());
-        Message message3 = new Message("Welcome !", subscription1,LocalDateTime.now());
-        Message message4 = new Message("Hi, Rayster!", subscription1,LocalDateTime.now());
+        Message message3 = new Message("Hi, Rayster!", subscription3,LocalDateTime.now());
+        Message message4 = new Message("Welcome!", subscription5,LocalDateTime.now());
+        Message message5 = new Message("Hi Zsolt!", subscription7,LocalDateTime.now());
 
         messageRepository.save(message1);
         messageRepository.save(message2);
         messageRepository.save(message3);
         messageRepository.save(message4);
+        messageRepository.save(message5);
     }
 
 }
