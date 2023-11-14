2acd0498d54e9dc3f6a91a43779e02cb94d0e7d6
Annie Wilson <annemarie.wilson@yahoo.com>
2023-11-14 10:16:52 +0000

    completed ChatRoom Service and Controller for the display chatroom by Id function

--- the_bright_network/src/main/java/com/bnta/the_bright_network/services/ChatRoomService.java
+++ the_bright_network/src/main/java/com/bnta/the_bright_network/services/ChatRoomService.java
@@ -1,4 +1,19 @@
 package com.bnta.the_bright_network.services;
 
+import com.bnta.the_bright_network.models.ChatRoom;
+import com.bnta.the_bright_network.repositories.ChatRoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class ChatRoomService {
+
+    @Autowired
+    ChatRoomRepository chatRoomRepository;
+
+    public Optional<ChatRoom> findChatRoomById(Long id){
+        return chatRoomRepository.findById(id);
+    }
 }
