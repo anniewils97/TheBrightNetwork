5e56b3de2ea849bf4dd8c83bd48a834bb105d8c0
Rayster Ramos <rjrfrst@gmail.com>
2023-11-14 16:14:59 +0000

    Displaying all users in the controller and service, complete

--- the_bright_network/src/main/java/com/bnta/the_bright_network/controllers/UserController.java
+++ the_bright_network/src/main/java/com/bnta/the_bright_network/controllers/UserController.java
@@ -1,4 +1,29 @@
 package com.bnta.the_bright_network.controllers;
 
+import com.bnta.the_bright_network.models.User;
+import com.bnta.the_bright_network.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping ("/users")
 public class UserController {
-}
+
+    @Autowired
+    UserService userService;
+
+
+    //Getting all users
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userService.displayAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+} //end
--- the_bright_network/src/main/java/com/bnta/the_bright_network/services/UserService.java
+++ the_bright_network/src/main/java/com/bnta/the_bright_network/services/UserService.java
@@ -1,4 +1,21 @@
 package com.bnta.the_bright_network.services;
 
+import com.bnta.the_bright_network.models.User;
+import com.bnta.the_bright_network.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
-}
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> displayAllUsers(){
+        return userRepository.findAll();
+    }
+
+
+} //End
