d9a9f2cfda879f0b507d4433178712b0c9f0fef5
Faiz Fazaluddin <faiz.mf07@gmail.com>
2023-11-14 17:41:14 +0000

    flipped .compareTo in comparator

--- the_bright_network/src/main/java/com/bnta/the_bright_network/services/ChatRoomService.java
+++ the_bright_network/src/main/java/com/bnta/the_bright_network/services/ChatRoomService.java
@@ -39,8 +39,8 @@ public class ChatRoomService {
             allMessages.addAll(subscriptionMessages);
         }
 
-        Collections.sort(allMessages, (a,b)->a.getTimeStamp().compareTo(b.getTimeStamp()));
-        Collections.reverse(allMessages);
+        Collections.sort(allMessages, (b,a)->a.getTimeStamp().compareTo(b.getTimeStamp()));
+
 
         ArrayList<MessageReplyDTO> orderedMessages = new ArrayList<>();
         for (Message message: allMessages) {
