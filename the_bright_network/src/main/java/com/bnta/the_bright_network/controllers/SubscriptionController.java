bb2ec99d50aa7f15a3febb622de38cc5636802ba
Sarah Ogunko <sarahogunko@hotmail.com>
2023-11-14 13:05:40 +0000

    corrected subscription service and controller

--- the_bright_network/src/main/java/com/bnta/the_bright_network/controllers/SubscriptionController.java
+++ the_bright_network/src/main/java/com/bnta/the_bright_network/controllers/SubscriptionController.java
@@ -1,6 +1,7 @@
 package com.bnta.the_bright_network.controllers;
 
 import com.bnta.the_bright_network.models.Subscription;
+import com.bnta.the_bright_network.models.SubscriptionDTO;
 import com.bnta.the_bright_network.services.SubscriptionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,12 +15,10 @@ public class SubscriptionController {
     @Autowired
     SubscriptionService subscriptionService;
 
-    @PatchMapping(value = "/{id}")
-    public ResponseEntity<Subscription> addPersonToChatroom(@RequestBody Subscription subscription, @PathVariable long id){
-        long userId = subscription.getId();
-        Subscription chatroom = subscriptionService.addNewUserToChatroom(id, userId);
+    @PostMapping
+    public ResponseEntity<Subscription> addPersonToChatroom(@RequestBody SubscriptionDTO subscriptionDTO){
+        Subscription chatroom = subscriptionService.addNewUserToChatroom(subscriptionDTO);
         return new ResponseEntity<>(chatroom, HttpStatus.OK);
-
 
     }
 }
--- the_bright_network/src/main/java/com/bnta/the_bright_network/services/SubscriptionService.java
+++ the_bright_network/src/main/java/com/bnta/the_bright_network/services/SubscriptionService.java
@@ -4,7 +4,9 @@ import com.bnta.the_bright_network.models.ChatRoom;
 import com.bnta.the_bright_network.models.Subscription;
 import com.bnta.the_bright_network.models.SubscriptionDTO;
 import com.bnta.the_bright_network.models.User;
+import com.bnta.the_bright_network.repositories.ChatRoomRepository;
 import com.bnta.the_bright_network.repositories.SubscriptionRepository;
+import com.bnta.the_bright_network.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,15 +19,20 @@ public class SubscriptionService {
     @Autowired
     SubscriptionRepository subscriptionRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ChatRoomRepository chatRoomRepository;
+
 //    adding a new user to the chatroom
     @Transactional
-    public Subscription addNewUserToChatroom(long chatroomId, long userId){
-        Subscription chatroom = subscriptionRepository.findById(chatroomId).get();
-         Subscription subscription = subscriptionRepository.findById(userId).get();
-        User user = subscription.getUser();
-         chatroom.setUser(user);
-         subscriptionRepository.save(chatroom);
-         return subscription;
+    public Subscription addNewUserToChatroom(SubscriptionDTO subscriptionDTO){
+        User user =  userRepository.findById(subscriptionDTO.getUserId()).get();
+        ChatRoom chatRoom = chatRoomRepository.findById(subscriptionDTO.getChatroomId()).get();
+        Subscription subscription = new Subscription(user, chatRoom);
+        subscriptionRepository.save(subscription);
+        return subscription;
 
 
 
