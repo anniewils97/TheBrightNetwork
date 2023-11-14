0e5062ddddf73eb71acba292c67612861a4b1770
Annie Wilson <annemarie.wilson@yahoo.com>
2023-11-14 15:42:28 +0000

    ordered big allMessages list in chronological order but now need to reverse the order

--- the_bright_network/src/main/java/com/bnta/the_bright_network/services/ChatRoomService.java
+++ the_bright_network/src/main/java/com/bnta/the_bright_network/services/ChatRoomService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,6 +36,7 @@ public class ChatRoomService {
             List<Message> subscriptionMessages = subscription.getMessages();
             allMessages.addAll(subscriptionMessages);
         }
+        Collections.sort(allMessages, (a,b)->a.getTimeStamp().compareTo(b.getTimeStamp()));
         return allMessages;
     }
 }
