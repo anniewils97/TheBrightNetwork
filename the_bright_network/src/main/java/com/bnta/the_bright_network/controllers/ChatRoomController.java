3a266e0c85adf2c059483203372447110665230f
Sarah Ogunko <sarahogunko@hotmail.com>
2023-11-14 12:01:44 +0000

    create function to add user to chatroom, and updates subscription controller with patchmapping

--- the_bright_network/src/main/java/com/bnta/the_bright_network/controllers/ChatRoomController.java
+++ the_bright_network/src/main/java/com/bnta/the_bright_network/controllers/ChatRoomController.java
@@ -30,3 +30,4 @@ public class ChatRoomController {
         } return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
+
--- the_bright_network/src/main/java/com/bnta/the_bright_network/services/SubscriptionService.java
+++ the_bright_network/src/main/java/com/bnta/the_bright_network/services/SubscriptionService.java
@@ -1,9 +1,35 @@
 package com.bnta.the_bright_network.services;
 
+import com.bnta.the_bright_network.models.ChatRoom;
+import com.bnta.the_bright_network.models.Subscription;
+import com.bnta.the_bright_network.models.SubscriptionDTO;
+import com.bnta.the_bright_network.models.User;
+import com.bnta.the_bright_network.repositories.SubscriptionRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class SubscriptionService {
+
+    @Autowired
+    SubscriptionRepository subscriptionRepository;
+
+//    adding a new user to the chatroom
+    @Transactional
+    public Subscription addNewUserToChatroom(long chatroomId, long userId){
+        Subscription chatroom = subscriptionRepository.findById(chatroomId).get();
+         Subscription subscription = subscriptionRepository.findById(userId).get();
+        User user = subscription.getUser();
+         chatroom.setUser(user);
+         subscriptionRepository.save(chatroom);
+         return subscription;
+
+
+
+    }
 
 
 
