a1b58d3c70dadcb60443f5f172addec1b8a3955b
Sarah Ogunko <sarahogunko@hotmail.com>
2023-11-16 16:02:36 +0000

    getMessageById

--- the_bright_network/src/main/java/com/bnta/the_bright_network/services/MessageService.java
+++ the_bright_network/src/main/java/com/bnta/the_bright_network/services/MessageService.java
@@ -78,8 +78,6 @@ public class MessageService {
 
     }
 
-
-
     //To get all messages
     public List<MessageReplyDTO> getAllMessages(){
         List<Message> messages = messageRepository.findAll();
@@ -107,6 +105,16 @@ public class MessageService {
             messageReplyDTOs.add(messageReply);
         }
         return messageReplyDTOs;
+    }
+
+    public Optional<Message> getMessageById(long id ){
+     Optional<Message> messages = messageRepository.findById(id);
+     if(messages.isEmpty()){
+         return null;
+     }
+     Message message = messages.get();
+     messageRepository.save(message);
+     return messages;
     }
 
 
